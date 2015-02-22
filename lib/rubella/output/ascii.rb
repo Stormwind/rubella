1ec11e4f5ca86e0325e8eb48e02730e48ce6acdb
Stormwind <stormwind@stormwinds-page.de>
2015-02-22 08:40:58 +0100

    Use a instance variable instead of static values for shades
    So I can provide different types of symbols for the ascii heatmap

--- lib/rubella/output/ascii.rb
+++ lib/rubella/output/ascii.rb
@@ -2,20 +2,17 @@ module Rubella
   module Output
 
     class ASCII
-      SHADE_0 = " "
-      SHADE_1 = "·"
-      SHADE_2 = "⚬"
-      SHADE_3 = "∞"
-      SHADE_4 = "@"
-      SHADE_5 = "#"
-      SHADE_6 = "░"
-      SHADE_7 = "▒"
-      SHADE_8 = "▓"
-      SHADE_9 = "█"
-
 
       def initialize field_size = 1
-        @field_size = field_size
+        @symbols = Hash.new
+        #@symbols["shades"] =
+        @symbols["shades_ascii"] =
+                            [" ", "·", "⚬", "∞", "@", "#", "░", "▒", "▓", "█"]
+        #@symbols["ascii"]   =
+        #@symbols["numbers"] =
+
+        @field_size   = field_size
+        @used_symbols = "shades_ascii"
       end
 
       def create parsed_list
@@ -33,29 +30,29 @@ module Rubella
         parsed_list.each do |point|
           i = 0
           point.reverse.each do |part|
-            part = (part*100).to_i
+            part = (part*100).to_i          
 
             case part
               when  0..10 then
-                ascii_arr[i] << SHADE_0
+                ascii_arr[i] << @symbols[@used_symbols][0]
               when 11..20 then
-                ascii_arr[i] << SHADE_1
+                ascii_arr[i] << @symbols[@used_symbols][1]
               when 21..30 then
-                ascii_arr[i] << SHADE_2
+                ascii_arr[i] << @symbols[@used_symbols][2]
               when 31..40 then
-                ascii_arr[i] << SHADE_3
+                ascii_arr[i] << @symbols[@used_symbols][3]
               when 41..50 then
-                ascii_arr[i] << SHADE_4
+                ascii_arr[i] << @symbols[@used_symbols][4]
               when 51..60 then
-                ascii_arr[i] << SHADE_5
+                ascii_arr[i] << @symbols[@used_symbols][5]
               when 61..70 then
-                ascii_arr[i] << SHADE_6
+                ascii_arr[i] << @symbols[@used_symbols][6]
               when 71..80 then
-                ascii_arr[i] << SHADE_7
+                ascii_arr[i] << @symbols[@used_symbols][7]
               when 81..90 then
-                ascii_arr[i] << SHADE_8
+                ascii_arr[i] << @symbols[@used_symbols][8]
               else
-                ascii_arr[i] << SHADE_9
+                ascii_arr[i] << @symbols[@used_symbols][9]
             end
             i = i+1
           end
