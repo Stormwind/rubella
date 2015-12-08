373c160d414a05c3dc2f8c7db38bc87a36df7e9b
Stormwind <stormwind@stormwinds-page.de>
2015-12-08 12:39:26 +0100

    Add a greyscale color theme to ColoredConsole

--- lib/rubella/output/colored_console.rb
+++ lib/rubella/output/colored_console.rb
@@ -29,6 +29,11 @@ module Rubella
         super data, field_size
 
         @symbols = Hash.new
+        @symbols["greyscale"] = self.create_colors [
+          232, 233, 234, 235, 236, 237, 238, 239,
+          240, 241, 242, 243, 244, 245, 246, 247,
+          248, 249, 250, 251, 252, 253, 254, 255
+        ]
         @symbols["colored_fields"] = self.create_colors [
             35+196,
             24+196,
